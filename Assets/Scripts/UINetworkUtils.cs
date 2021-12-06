f24244ecc8f0cebc4686ebabd9c16e5c814071bc
Dilmer Valecillos <dilmervst@gmail.com>
2021-12-05 20:21:44 -0700

    XRMultiplayerMain will now auto create a host or client

--- Assets/Scripts/UINetworkUtils.cs
+++ Assets/Scripts/UINetworkUtils.cs
@@ -1,32 +1,17 @@
 using Unity.Netcode;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UINetworkUtils : MonoBehaviour
 {
-    [SerializeField]
-    private Button startHost;
-
-    [SerializeField]
-    private Button startClient;
-
-    // Start is called before the first frame update
     void Start()
     {
-        startHost.onClick.AddListener(() =>
+        if (NetworkLobby.Instance.CollaboratorsConnected.Value == 0)
         {
             NetworkManager.Singleton.StartHost();
-        });
-
-        startClient.onClick.AddListener(() =>
+        }
+        else
         {
             NetworkManager.Singleton.StartClient();
-        });
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
