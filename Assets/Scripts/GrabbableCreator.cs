745a74dfd86e53618a11409fcaea8d84c31b07bc
Dilmer Valecillos <dilmervst@gmail.com>
2021-12-06 20:45:00 -0700

    Fixing few things

--- Assets/Scripts/GrabbableCreator.cs
+++ Assets/Scripts/GrabbableCreator.cs
@@ -10,6 +10,9 @@ public class GrabbableCreator : NetworkBehaviour
     private Vector2 placementArea = new Vector2(-10.0f, 10.0f);
 
     [SerializeField]
+    private Vector2 prefabSizes = new Vector2(0.01f, 2.0f);
+
+    [SerializeField]
     private int maxObjectsToSpawn = 10;
 
     void Start()
@@ -18,11 +21,12 @@ public class GrabbableCreator : NetworkBehaviour
         {
             for (int i = 0; i < maxObjectsToSpawn; i++)
             {
-                //prefabs[Random.Range(0, prefabs.Length)]
-                GameObject go = Instantiate(prefabs[0], Vector3.zero, Quaternion.identity);
-                go.transform.position = new Vector3(Random.Range(placementArea.x, placementArea.y), 0,
+                GameObject go = Instantiate(prefabs[Random.Range(0, prefabs.Length)], Vector3.zero, Quaternion.identity);
+                go.transform.position = new Vector3(Random.Range(placementArea.x, placementArea.y), 5,
                     Random.Range(placementArea.x, placementArea.y));
-
+                
+                float randomSize = Random.Range(prefabSizes.x, prefabSizes.y);
+                go.transform.localScale = new Vector3(randomSize, randomSize, randomSize);
                 go.GetComponent<NetworkObject>().Spawn();
             }
         }
